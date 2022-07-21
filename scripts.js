0620d34cb85fc1e5ecdeba12bb09f5ad661977f3
Brooks Susor <susorbrooks@gmail.com>
2022-07-21 11:24:36 -0400

    Add display value

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
-const display = document.querySelector('.screen'); 
+//variables for calc setup
+const display = document.querySelector('.display'); 
 const row1 = document.querySelector('.row-1');
 const row2 = document.querySelector('.row-2');
 const row3 = document.querySelector('.row-3');
 const row4 = document.querySelector('.row-4'); 
 const arr = [display, row1, row2, row3, row4]; 
+const displayValue = 0; 
 
 for (let i = 1; i < arr.length; i++) {
   for (let j = 0; j < 5; j++) {
     const btn = document.createElement('button'); 
     btn.style.flex = '1'; 
     btn.style.margin = '3px'; 
-    btn.style.backgroundColor = 'red'; 
+    btn.style.backgroundColor = 'gray'; 
     arr[i].appendChild(btn); 
   }
 }
+
+display.textContent = displayValue; 
