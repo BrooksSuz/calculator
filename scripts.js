828f1f219a229d93c3b7f1fa47bb4ffa4b493e5a
Brooks Susor <susorbrooks@gmail.com>
2022-07-21 10:39:55 -0400

    Create basic shape of calc

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const display = document.querySelector('.screen'); 
+const row1 = document.querySelector('.row-1');
+const row2 = document.querySelector('.row-2');
+const row3 = document.querySelector('.row-3');
+const row4 = document.querySelector('row-4'); 
+const arr = [display, row1, row2, row3, row4]; 
+
+for (let i = 0; i < 5; i++) {
+  const button = document.createElement('div'); 
+
+}
