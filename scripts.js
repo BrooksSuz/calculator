72c0e0e4ac4c5ee108a0fefa3032cd1ca4bd0230
Brooks Susor <susorbrooks@gmail.com>
2022-07-25 19:46:53 -0400

    Update digit functionality

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 //variables for calc setup
 const btns = document.querySelectorAll('button'); 
 const display = document.querySelector('.display'); 
+const nums = /[0-9]/; 
+const mdas = /x|\/|\+|\-/;
 let displayValue = 0; 
 
 display.textContent = displayValue; 
 
 btns.forEach(btn => {
-  btn.addEventListener('click', function displayNumber() {
-    displayValue = btn.textContent; 
-    display.textContent = displayValue; 
+  btn.addEventListener('click', function displayNumber(e) {
+    if (e.target.textContent.match(nums)) {
+      displayValue = btn.textContent; 
+      display.textContent = displayValue;
+    }
   }); 
 });
