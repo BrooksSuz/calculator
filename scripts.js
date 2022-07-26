0555713becedf09f43fc09886e974ec9a1347105
Brooks Susor <susorbrooks@gmail.com>
2022-07-25 20:21:57 -0400

    Begin creating operator functions

--- scripts.js
+++ scripts.js
@@ -3,15 +3,27 @@ const btns = document.querySelectorAll('button');
 const display = document.querySelector('.display'); 
 const nums = /[0-9]/; 
 const mdas = /x|\/|\+|\-/;
-let displayValue = 0; 
-
-display.textContent = displayValue; 
+const arr = []; 
+let currentValue; 
+let displayValue;  
 
 btns.forEach(btn => {
   btn.addEventListener('click', function displayNumber(e) {
     if (e.target.textContent.match(nums)) {
-      displayValue = btn.textContent; 
-      display.textContent = displayValue;
+      currentValue = btn.textContent; 
+      display.textContent += currentValue;
+      displayValue = display.textContent; 
+    } else if (e.target.textContent.match(mdas)) {
+      if (e.target.textContent === '+') {
+        add(); 
+      }
     }
   }); 
 });
+
+function add() {
+  arr.push(displayValue); 
+  if (arr.length = 2) {
+    arr = arr.reduce((a, b) => a + b); 
+  }
+} 
